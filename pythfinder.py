6a4364f3a1e131c42971e7b30611132a55bcd56c
Marthin- <martin.guilloux@gmail.com>
2016-11-03 17:04:53 +0100

    equipement

--- pythfinder.py
+++ pythfinder.py
@@ -19,7 +19,7 @@ class dice:
     def roll(self):
         val = self.value
         crit = self.crit
-        r = random.randint(1,val)
+        r = random.randint(1, val)
         if r >= crit:
             print("critical hit !")
         if r == 1:
@@ -35,7 +35,22 @@ class arme:
         self.nb_dice = nbd
         self.crit = crit
         self.crit_mul = critmul
-        self.main = main+1
+        self.main = main + 1
+
+
+############# CLASSE ÉQUIPÉ (OBJETS PORTÉS) ############
+class Equipe:
+    def __init__self(self, md, armure, mg, tete, front, yeux, epaules, corps, torse, poignets, anneau1, anneau2, cou,
+                     taille, pieds):
+        # dictionnaire du stuff équipé
+        self.slot = {'tete': tete, 'front': front, 'yeux': yeux,
+                     'epaules': epaules, 'corps': corps, 'torse': torse,
+                     'poignets': poignets, 'mg': mg, 'md': md,
+                     'anneau1': anneau1, 'anneau2': anneau2, 'armure': armure,
+                     'cou': cou, 'taille': taille, 'pieds': pieds}
+
+    def set_item(self, place, stuff):
+        self.slot[place] = stuff
 
 
 ############# CLASSE ARMURE ##########
@@ -54,69 +69,72 @@ class stuff:
 
 ############# CLASSE PERSONNAGE #############
 class perso:
-
-# init
+    # init
     def __init__(self, name, mbba=0, mlvl=1,
                  forc=10, dex=10, con=10, inte=10, sag=10, cha=10,
                  vig=0, ref=0, vol=0,
                  arme=None, arme2=None, armure=None, armure2=None):
         self.name = name
-        self.stats = [forc,dex,con,inte,sag,cha]
-        self.stats_mod = [0,0,0,0,0,0]
+        self.stats = [forc, dex, con, inte, sag, cha]
+        self.stats_mod = [0, 0, 0, 0, 0, 0]
         self.lvl = mlvl
         self.bba = mbba
         self.dons = []
-        self.defenses=[10,0,0,0] #CA, ref, vig, vol
+        self.defenses = [10, 0, 0, 0]  # CA, ref, vig, vol
         self.armes = []
         self.armures = []
         self.stuff = []
+        self.equipement = Equipe()
+
         if arme is not None:
             self.armes.append(arme)
             if arme2 is not None:
                 self.armes.append(arme)
         if armure is not None:
-            self.defenses[0]+=armure.value
+            self.defenses[0] += armure.value
             self.armures.append(armure)
             if armure2 is not None:
-                self.defenses[0]+=armure2.value
+                self.defenses[0] += armure2.value
                 self.armures.append(armure2)
-    #ajouter arme
-    def add_armes(self,arme):
+
+    # ajouter arme
+    def add_armes(self, arme):
         self.armes.append(arme)
-    #ajouter armure
-    def add_armure(self,armure):
+
+    # ajouter armure
+    def add_armure(self, armure):
         self.armures.append(armure)
-    #calculer et lancer jet d'attaque
-    def atk(self,arme,mode=0,other=0):
-        bonus=self.bba
+
+    # calculer et lancer jet d'attaque
+    def atk(self, arme, mode=0, other=0):
+        bonus = self.bba
         togo = bonus
-        if mode == 0 :#pas de don
-            bonus+=get_mod(self.stats[0]+self.stats_mod[0])
-        elif mode == 1 :#attaque en puissance
-            bonus+=(arme.main / 2) * (get_mod(self.stats[0] + self.stats_mod[0])) - (self.bba / 4)
-        elif mode == 2 :#distance/attaque en finesse
-            bonus+=get_mod(self.stats[1]+self.stats_mod[1])
-        bonus+=other
-        result=[]
+        if mode == 0:  # pas de don
+            bonus += get_mod(self.stats[0] + self.stats_mod[0])
+        elif mode == 1:  # attaque en puissance
+            bonus += (arme.main / 2) * (get_mod(self.stats[0] + self.stats_mod[0])) - (self.bba / 4)
+        elif mode == 2:  # distance/attaque en finesse
+            bonus += get_mod(self.stats[1] + self.stats_mod[1])
+        bonus += other
+        result = []
         while togo >= 0:
-            d=dice()
-            result.append(d.roll()+bonus-1)#magic number yolo. It works.
-            togo-=6
-            bonus-=6
+            d = dice()
+            result.append(d.roll() + bonus - 1)  # magic number yolo. It works.
+            togo -= 6
+            bonus -= 6
         return result
-    # resoudre defense
-        def defense(self,test=0):#test : place dans tableau defenses
-            d=dice()
+
+        # resoudre defense
+        def defense(self, test=0):  # test : place dans tableau defenses
+            d = dice()
             if test == 0:
                 return self.defenses[0]
             elif test == 1:
-                return d.roll()+get_mod(self.defenses[test]+self.stats[1])
+                return d.roll() + get_mod(self.defenses[test] + self.stats[1])
             elif test == 2:
-                return d.roll()+get_mod(self.defenses[test]+self.stats[2])
+                return d.roll() + get_mod(self.defenses[test] + self.stats[2])
             elif test == 3:
-                return d.roll()+get_mod(self.defenses[test]+self.stats[4])
-
-
+                return d.roll() + get_mod(self.defenses[test] + self.stats[4])
 
 
 def shell():
@@ -128,20 +146,21 @@ def shell():
             print("It works !")
         elif comm[0] == "add_user":
             if len(comm) is not 4:
-                print("error with arguments (usage : "+comm[0]+" <character name> <username> <role>")
+                print("error with arguments (usage : " + comm[0] + " <character name> <username> <role>")
             else:
                 try:
-                    db.add_user(comm[1],comm[2],comm[3])
+                    db.add_user(comm[1], comm[2], comm[3])
                 except sqlite3.DatabaseError:
                     print("could not add user to database !")
 
         elif comm[0] == "add_perso":
             if len(comm) is not 9:
-                print("error with arguments (usage : " + comm[0] + " <nom_perso> <race> <force> <dexterite> <constitution> <intelligence> <sagesse> <charisme>")
+                print("error with arguments (usage : " + comm[
+                    0] + " <nom_perso> <race> <force> <dexterite> <constitution> <intelligence> <sagesse> <charisme>")
             else:
                 try:
-                    #TODO prendre race en compte
-                    db.add_perso(comm[1],comm[3],comm[4],comm[5],comm[6],comm[7],comm[8])
+                    # TODO prendre race en compte
+                    db.add_perso(comm[1], comm[3], comm[4], comm[5], comm[6], comm[7], comm[8])
                 except sqlite3.DatabaseError:
                     print("could not add character to database !")
 
@@ -159,23 +178,25 @@ def shell():
                 print("error with arguments : usage : atk -p=<perso> -a=<arme> -m=<mode> -b=<bonus/malus>")
             else:
                 print("en cours d'implé")
-                print("perso : %s, arme : %s, mode : %s, bonus : %s" %(comm[1],comm[2],comm[3],comm[4],))
+                print("perso : %s, arme : %s, mode : %s, bonus : %s" % (comm[1], comm[2], comm[3], comm[4],))
 
         elif comm[0] == "exit":
             print("Exiting pythfinder shell...")
             break
+
 
 if __name__ == '__main__':
     print("start")
     db = Database(True)
     db.update_weapons()
 
-    db.add_user('fafa','lolo','testeur')
-    db.add_perso('fafa',12,10,20,3,2,6)
+    db.add_user('fafa', 'lolo', 'testeur')
+    db.add_perso('fafa', 12, 10, 20, 3, 2, 6)
     db.get_carac('fafa')
-    pj = perso("fonzie", 1,0,12,10,16,14,14,18,0,0,0)
+    pj = perso("fonzie", 1, 0, 12, 10, 16, 14, 14, 18, 0, 0, 0)
+    pj.equipement=Equipe(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0,0)
     print(pj.name)
-    print("force : "+str(pj.stats[0]))
+    print("force : " + str(pj.stats[0]))
     attack = pj.atk(arme())
     print("jet d'attaque : " + str(attack))
     shell()
